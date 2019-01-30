04003e62fdb50e0cdc52a7472c7c40c2fbcb4012
Jose Guevara <jguevara@pros.com>
2019-01-30 03:09:16 -0400

    Add solution for Credit Card Issuer problem

---
+++ src/credit-card-issuer/jose/CreditCardIssuer.java
@@ -0,0 +1,31 @@
+public class CreditCardIssuer {
+
+    public static void main(String[] args) {
+        System.out.println(getIssuer(args[0]));
+    }
+
+    public static String getIssuer(String cardNumber) {
+        String cardType = "Unknown";
+        if (cardNumber.length() == 15 &&
+            (cardNumber.indexOf("34") == 0 ||
+            cardNumber.indexOf("37") == 0)) {
+            cardType = "AMEX";
+        } else if (cardNumber.length() == 16 || 
+            cardNumber.length() == 13) {
+            if (cardNumber.indexOf("4") == 0) {
+                cardType = "VISA";
+            } else if (cardNumber.length() == 16) {
+                if (cardNumber.indexOf("6011") == 0) {
+                    cardType = "Discover";
+                } else if (cardNumber.indexOf("51") == 0 ||
+                    cardNumber.indexOf("52") == 0 ||
+                    cardNumber.indexOf("53") == 0 ||
+                    cardNumber.indexOf("54") == 0 ||
+                    cardNumber.indexOf("55") == 0) {
+                    cardType = "Mastercard";
+                }
+            }
+        }
+        return cardType;
+    }
+}
