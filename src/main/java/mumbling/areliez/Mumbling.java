69eed2f99194a4a8add943d1877d96febfa9ed20
Areliez Vargas <areliez.vargas@gmail.com>
2019-02-06 09:31:26 -0400

    resolve exercise mmumbling

---
+++ src/main/java/mumbling/areliez/Mumbling.java
@@ -0,0 +1,30 @@
+package mumbling.areliez;
+
+public class Mumbling {
+
+    public String accum(String word) {
+        int length = word.length();
+        String ini = "";
+        for (int i = 0; i <= length; i++) {
+            String character = String.valueOf(word.charAt(i));
+            String guion = "-";
+            int numberLeter = i++;
+            String newWord =
+                    accum(character, numberLeter, guion);
+        }
+        // return what?
+        return null;
+    }
+
+    private String accum(String character, int numberLeter, String guion) {
+        String word= "";
+        for (int j = 1; j <= numberLeter; j++) {
+            if (j==1) {
+                String wordAccum = character.toUpperCase();
+                word = word + wordAccum;
+            } else {
+                word = word + character.toLowerCase();
+            }
+        }return word + guion;
+    }
+}
