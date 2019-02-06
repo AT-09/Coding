2ffd9f22f00b28d8e6cf47e7dd4143d23b73a1b7
AreliezVargas <areliez.v25@gmail.com>
2019-02-06 18:19:32 -0400

    Refactor code

--- src/main/java/disemvoweltrolls/areliez/Disemvowltrolls.java
+++ src/main/java/disemvoweltrolls/areliez/Disemvowltrolls.java
@@ -1,42 +1,44 @@
 package disemvoweltrolls.areliez;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Disemvowltrolls {
 
-    public static String removedVowels(String sentence) {
+    public static String removeVowels(String sentence) {
         if (sentence.length()>0) {
-            return removedVowels(sentence, 0);
+            return removeAllVowels(sentence, 0);
         }
-        return "the sentence is short";
+        return "Enter a sentence";
     }
 
-    private static String removedVowels(String sentence, int position) {
+    /**
+     * Remove all vowels in the sentence.
+     * @param sentence the sentence.
+     * @param position the position to start a searching
+     * @return a string without vowels.
+     */
+    private static String removeAllVowels(String sentence, int position) {
         String stringResult = "";
-        for (int i=0;i < sentence.length();i++) {
-            String character = String.valueOf(sentence.charAt(i));
-            if (isVowel(character)) {
-                stringResult= stringResult;// + sentence.replace(character,'+');
+        for (int i = position; i < sentence.length(); i++) {
+            String letter = String.valueOf(sentence.charAt(i));
+            if (isVowel(letter)) {
+                stringResult= stringResult;
             }
             else {
-                stringResult = stringResult + character;
+                stringResult = stringResult + letter;
             }
         }return stringResult;
     }
 
-    private static boolean isVowel(String character) {
-        String vowelA = "A";
-        String vowela = "a";
-        String vowele = "e";
-        String vowelE = "E";
-        String voweli = "i";
-        String vowelI = "I";
-        String vowelO = "O";
-        String vowelo = "o";
-        String vowelU = "U";
-        String vowelu = "u";
-        if (character.equals(vowelA) || character.equals(vowela) || character.equals(vowelE)) {
-        return true;
-        } else return false;
+    /**
+     * Verify if the letter is a uppercase or lowercase vowel.
+     * @param letter the string to verify if is vowel.
+     * @return true if letter is vowel, false otherwise false.
+     */
+    private static boolean isVowel(String letter) {
+        Pattern vowelPattern = Pattern.compile("[aeiouAEIOU]");
+        Matcher vowelMatcher = vowelPattern.matcher(letter);
+        return vowelMatcher.find();
     }
-
-
 }
--- src/test/java/disemvowltrolls/areliez/DisemvowltrollsTest.java
+++ src/test/java/disemvowltrolls/areliez/DisemvowltrollsTest.java
@@ -8,6 +8,6 @@ public class DisemvowltrollsTest {
 
     @Test
     public void testOne() {
-        Assert.assertEquals(Disemvowltrolls.removedVowels("This website is for losers LOL!"),"Ths wbst s fr lsrs LL!");
+        Assert.assertEquals(Disemvowltrolls.removeVowels("This website is for losers LOL!"),"Ths wbst s fr lsrs LL!");
     }
 }
